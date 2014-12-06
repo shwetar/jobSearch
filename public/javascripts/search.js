ba36fa95ca5e0cb17348d0986922bb6612b5c291
Oleg Polyakov <opolyo01@yahoo.com>
2014-12-06 12:54:09 -0800

    UI changes

---
+++ public/javascripts/search.js
@@ -0,0 +1,8 @@
+$(".search").on("click", function(ev){
+    ev.preventDefault();
+
+    $.get( "/listings/search", { title: $("#title").val(), location: $("#location").val() } )
+      .done(function( data ) {
+        console.log( "Data Loaded: " + data );
+    });
+});
--- routes/listings.js
+++ routes/listings.js
@@ -2,9 +2,21 @@ var express = require('express');
 var router = express.Router();
 var listingsModel = require("../model/listings");
 
-/* GET login page. */
+/* GET listings page. */
 router.get('/', function(req, res) {
-  res.render('search',{ title: 'Search Page' });
+  res.render('listings',{ title: 'Listings Page' });
+});
+
+router.get('/search', function(req, res) {
+  res.send([{
+    title: "xyz",
+    location: "abc",
+    description: "desc"
+  },{
+    title: "xyz",
+    location: "abc",
+    description: "desc"
+  }]);
 });
 
 router.post('/',function(req,res,next){
