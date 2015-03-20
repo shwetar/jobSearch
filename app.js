82a9ad59844e0eaa20cf3de0fefa03055b11fe85
Oleg Polyakov <opolyo01@yahoo.com>
2015-03-20 08:29:45 -0700

    update home screen

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Need to update Home screen
 app.get('/', function(req, res){
     res.send("Hello world");
 });
